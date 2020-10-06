de34c531ca9e70be132813bd8532917738f0acfd
John Sorhannus <jsorhannus@gmail.com>
2020-10-06 16:03:40 -0400

    Created analyzevideos.py and ytanalyzer.py

---
+++ analyzevideos.py
@@ -0,0 +1,7 @@
+import ytanalyzer as yt
+
+def main():
+	yt.download_videos()
+
+if __name__ == "__main__":
+	main()
---
+++ ytanalyzer.py
@@ -0,0 +1,8 @@
+#from pytube import YouTube
+
+def download_videos():
+	#yt = YouTube('https://www.youtube.com/watch?v=NIWwJbo-9_8&ab_channel=CoreySchafer')
+	print("Hello world!")
+
+def test():
+	print("testing this function")
