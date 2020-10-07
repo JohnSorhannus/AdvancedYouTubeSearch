1eb7e4a9ed96c6710d82ff0a5c102034aeaa51b7
John Sorhannus <jsorhannus@gmail.com>
2020-10-07 15:57:37 -0400

    Create Video model

--- search/models.py
+++ search/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+	upload_date = models.CharField(max_length=10)
+	user = models.CharField(max_length=200)
+	captions = models.TextField(null=True) #some videos may not contain captions
+	thumbnail_url = models.TextField()
+	video_url = models.TextField()
+	length = models.CharField(max_length=200)
+	views = models.CharField(max_length=200)
+
