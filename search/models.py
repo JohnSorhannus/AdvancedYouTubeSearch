45f7caef2a5916c9b0cd1346d8532fb878803dfb
John Sorhannus <jsorhannus@gmail.com>
2020-10-07 16:03:22 -0400

    Changed 'description' field in Video model to be of type 'TextField'

--- search/models.py
+++ search/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Video(models.Model):
 	title = models.CharField(max_length=200)
-	description = models.CharField(max_length=200)
+	description = models.TextField()
 	upload_date = models.CharField(max_length=10)
 	user = models.CharField(max_length=200)
 	captions = models.TextField(null=True) #some videos may not contain captions
@@ -11,4 +11,7 @@ class Video(models.Model):
 	video_url = models.TextField()
 	length = models.CharField(max_length=200)
 	views = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.title
 
