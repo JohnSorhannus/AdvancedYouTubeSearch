b9fe5948b747455fb85336436deb79accf18c2ec
John Sorhannus <jsorhannus@gmail.com>
2020-10-09 19:51:32 -0400

    Wrote function to extract captions from XML and convert captions into a string for database insertion

--- search/management/commands/ytanalyzer.py
+++ search/management/commands/ytanalyzer.py
@@ -7,19 +7,42 @@ Contains functions that extract meta data and objects from videos and inserts da
 from pytube import YouTube
 from pytube import extract
 from search.models import Video
+import xml.etree.ElementTree as ET
+import re
+import html
 
 def download_videos():
-	video = YouTube('https://www.youtube.com/watch?v=At3xcj-pTjg&ab_channel=CaseyNeistat')
+	video = YouTube('https://www.youtube.com/watch?v=DyUrqZBs2XA&ab_channel=CaseyNeistat')
+
+	try:
+		xml_captions = video.captions['en'].xml_captions
+		str_captions = extract_captions(xml_captions)
+	except:
+		str_captions = ''
 
 	entry = Video(title=video.title,
 		description=video.description,
-		upload_date='2016-12-20',
+		upload_date='2016-12-21',
 		user=video.author,
-		captions='',
+		captions=str_captions,
 		thumbnail_url=video.thumbnail_url,
-		video_url='https://www.youtube.com/watch?v=At3xcj-pTjg&ab_channel=CaseyNeistat',
+		video_url='https://www.youtube.com/watch?v=DyUrqZBs2XA&ab_channel=CaseyNeistat',
 		length=video.length,
 		views=video.views,
-		video_id=extract.video_id('https://www.youtube.com/watch?v=At3xcj-pTjg&ab_channel=CaseyNeistat'))
+		video_id=extract.video_id('https://www.youtube.com/watch?v=DyUrqZBs2XA&ab_channel=CaseyNeistat'))
 
 	entry.save()
+
+def extract_captions(xml):
+	root = ET.fromstring(xml)
+	caption = ''
+
+	for s in root.iter('text'):
+		sen = s.text
+		print(sen)
+		matches = re.findall("&(?:[a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});", sen)
+		for item in matches:
+			sen = re.sub(item, html.unescape(item), sen)
+		caption += sen + ' '
+
+	return caption
