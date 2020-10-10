599dd1df6cb9e62a67a146e439425754dbaddc9d
John Sorhannus <jsorhannus@gmail.com>
2020-10-10 12:36:24 -0400

    Renamed 'ytanalyzer.py' to 'extract_metadata.py', added field to enter in upload date in 'home.html', wrote function to extract upload date for video, wrote code to download video into 'videos' directory

--- search/management/commands/analyzevideos.py
+++ search/management/commands/analyzevideos.py
@@ -4,10 +4,10 @@ analyzevideos.py
 Script to be executed that calls functions that analyzes videos. Meant to be executed before running django server for the first time.
 """
 
-import search.management.commands.ytanalyzer as yt
+import search.management.commands.extract_metadata as emd
 from django.core.management import BaseCommand
 
 class Command(BaseCommand):
 	"""docstring for Command"""
 	def handle(self, **options):
-		yt.download_videos()
+		emd.download_videos()
